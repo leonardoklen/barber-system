44cad7aafaba6d696d7c9fb6d55b274570d7aaae
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-07-09 13:11:03 -0300

    FEAT: Criação de Migration para Agendamentos

---
+++ src/database/migrations/2022_07_09_142205_create_scheduling_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scheduling', function (Blueprint $table) {
+            $table->id();
+            $table->datetime('datetime');
+            $table->string('service');
+            $table->string('name');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scheduling');
+    }
+};
