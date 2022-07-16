6702129c1602672992a3833c079497cd34d87ac6
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-07-16 01:16:33 -0300

    FEAT: Atualizado versão do jquery

--- src/resources/views/components/website/footer.blade.php
+++ src/resources/views/components/website/footer.blade.php
@@ -1,5 +1,5 @@
 </body>
 
-<script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
