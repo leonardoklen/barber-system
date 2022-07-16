36120554738871e44a717cc644a00207d2c7287c
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-07-16 02:34:35 -0300

    FIX: Ajustado conflito de merge

--- src/resources/views/components/website/footer.blade.php
+++ src/resources/views/components/website/footer.blade.php
@@ -1,9 +1,5 @@
 </body>
 
-<<<<<<< HEAD
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-=======
 <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
->>>>>>> 4d6da5511b5f1628c832408094774840a3bec2f4
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
