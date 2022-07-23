411dc8656ad5ba4f060ba63e439043fee0e56154
Leonardo Klen <leoklen203@gmail.com>
2022-07-23 17:11:22 -0300

    FEAT: Criado entity do módulo scheduling

---
+++ src/public/assets/js/api/Scheduling/SchedulingEntity.js
@@ -0,0 +1,33 @@
+export class SchedulingEntity {
+    dateTime;
+    idService;
+    name;
+    phone;
+
+    constructor(data) {
+        this.#populateScheduling(data);
+    }
+
+    #populateScheduling(data) {
+        this.dateTime = data.dateTime;
+        this.idService = data.idService;
+        this.name = data.name;
+        this.phone = data.phone;
+    }
+
+    getDateTime() {
+        return this.dateTime;
+    }
+
+    getIdService() {
+        return this.idService;
+    }
+
+    getName() {
+        return this.name;
+    }
+
+    getPhone() {
+        return this.phone;
+    }
+}
