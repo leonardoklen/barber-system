df637901dc8cbb5a6e794416175ccc8c4fdc9b37
Vinicius Costa <vinicius.costa@gazin.com.br>
2022-07-31 15:09:02 -0300

    FEAT: Criado sessão e ajuste no retorno do método login()

--- src/app/Http/Controllers/UserController.php
+++ src/app/Http/Controllers/UserController.php
@@ -5,18 +5,42 @@ namespace App\Http\Controllers;
 use App\Models\User;
 use Illuminate\Http\Request;
 
+session_start();
+
 class UserController extends Controller
 {
-    public function login(Request $request) {
+    public function login(Request $request)
+    {
         try {
             $user = User::where('email', $request->query('email'))->where('password', $request->query('password'))->first();
-            if($user) {
-                return response()->json(["message" => "Login Autorizado!"], 200);
+
+            if ($user) {
+                $this->initSession($user->name);
+                return response()->json(["message" => true], 200);
             } else {
-                return response()->json(["message" => "E-mail ou senha incorretos!"], 404);
+                return response()->json(["message" => false], 404);
             }
-        } catch(\Throwable $error) {
+        } catch (\Throwable $error) {
             return response()->json(["message" => "Falha ao tentar realizar login: " . $error->getMessage()], 500);
         }
     }
+
+    public function initSession($userName)
+    {
+        $_SESSION['userName'] = $userName;
+    }
+
+    public function sessionDestroy() {
+        session_destroy();
+    }
+
+    public static function checkIfSessionExists($location) {
+        if($location != '/entrar') {
+            if(!isset($_SESSION['userName'])) {
+                header("Location: " . $_SERVER['HTTP_HOST'] . "/entrar");
+                exit;
+            }
+        }
+    }
+
 }
