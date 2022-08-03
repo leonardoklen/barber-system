45d0198c081d62558f007567728e962388dab392
Leonardo Klen <leoklen203@gmail.com>
2022-08-02 21:32:14 -0300

    feat: criado navbar e add lib bootstrap icon

--- src/resources/views/components/website/head.blade.php
+++ src/resources/views/components/website/head.blade.php
@@ -16,6 +16,21 @@ date_default_timezone_set('America/Sao_Paulo');
 
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css" integrity="sha384-eoTu3+HydHRBIjnCVwsFyCpUDZHZSFKEJD0mc3ZqSBSb6YhZzRHeiomAUWCstIWo" crossorigin="anonymous">
 </head>
 
 <body class="bg-light">
+
+    <nav class="navbar navbar-dark bg-dark shadow">
+      <div class="container-fluid text-center justify-content-center">
+        <a class="navbar-brand m-0 ms-2 me-2 fs-2" href="#">
+            <i class="bi bi-instagram"></i>
+        </a>
+        <a class="navbar-brand m-0 ms-2 me-2 fs-2" href="#">
+            <i class="bi bi-facebook"></i>
+        </a>
+        <a class="navbar-brand m-0 ms-2 me-2 fs-2" href="#">
+            <i class="bi bi-whatsapp"></i>
+        </a>
+      </div>
+    </nav>
