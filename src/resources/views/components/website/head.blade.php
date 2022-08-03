dbed1c1580f46b3441dd4fe43a9403a8e90d838f
Leonardo Klen <leoklen203@gmail.com>
2022-08-02 22:06:34 -0300

    feat: adicionado icon e titulo browser

--- src/resources/views/components/website/head.blade.php
+++ src/resources/views/components/website/head.blade.php
@@ -12,7 +12,9 @@ date_default_timezone_set('America/Sao_Paulo');
     <meta name="description" content="" />
     <meta name="author" content="" />
 
-    <title>Sistema</title>
+    <title>Barbearia Queiroz</title>
+
+    <link rel="icon" href="assets/img/logo.jfif">
 
     <link href="https://fonts.googleapis.com/css?family=Roboto+Slab:400,100,300,700" rel="stylesheet" type="text/css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
