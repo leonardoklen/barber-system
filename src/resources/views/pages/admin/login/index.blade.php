73db3ba52471f3e325343bb54219998bf72bf5af
Leonardo Klen <leoklen203@gmail.com>
2022-08-03 23:50:23 -0300

    feat: nova tela de login somente com bootstrap 5

--- src/resources/views/pages/admin/login/index.blade.php
+++ src/resources/views/pages/admin/login/index.blade.php
@@ -1,57 +1,67 @@
-@extends('components.admin.head')
-@section('title', 'Entrar')
+@php
+    use App\Http\Controllers\UserController;
+    UserController::checkIfSessionExists($_SERVER['REQUEST_URI']);
+@endphp
 
-<body class="bg-gradient-light">
+<!DOCTYPE html>
+<html lang="pt-BR">
 
+<head>
+    
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Barber System</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.3/font/bootstrap-icons.css" integrity="sha384-eoTu3+HydHRBIjnCVwsFyCpUDZHZSFKEJD0mc3ZqSBSb6YhZzRHeiomAUWCstIWo" crossorigin="anonymous">
+
+    <link href="assets/css/pages/admin/login/index.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<body class="vh-100">
     <div class="container h-100">
-
-        <div class="row justify-content-center h-100 align-items-center">
-
-            <div class="col-xl-10 col-lg-12 col-md-9">
-
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <div class="row">
-                            <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-                            <div class="col-lg-6">
-                                <div class="p-5">
-                                    <br><br><br>
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Bem-vindo de volta!</h1>
-                                    </div>
-                                    <form class="user">
-                                        <div class="form-group">
-                                            <input type="email" class="form-control form-control-user" id="emailLogin" aria-describedby="emailHelp" placeholder="E-mail">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user" id="passwordLogin" placeholder="Senha">
-                                        </div>
-                                        <div class="form-group">
-                                            <div class="custom-control custom-checkbox small">
-                                                <input type="checkbox" class="custom-control-input" id="customCheck">
-                                                <label class="custom-control-label" for="customCheck">Lembrar-me</label>
-                                            </div>
-                                        </div>
-                                        <a type="none" id="btnLogin" class="btn btn-primary btn-user btn-block">
-                                            Entrar
-                                        </a>
-                                    </form>
-                                    <hr>
-                                    <div class="text-center">
-                                        <a class="small" href="esqueceu-senha">Esqueceu sua senha?</a>
-                                    </div>
-                                    <br><br><br>
-                                </div>
-                            </div>
+        <div class="row justify-content-center align-items-center h-100">
+            <div class="col-12">
+                <div class="d-flex justify-content-center border-0 shadow-lg">
+                    <div id="divCard" class="p-5">                                    
+                        <div class="text-center mb-4">
+                            <h4>Barber System</h4>
                         </div>
+                        <form class="user">
+                            <div class="form-group mb-3">
+                                <input type="email" class="form-control" id="emailLogin" aria-describedby="emailHelp" placeholder="E-mail">
+                            </div>
+                            <div class="form-group mb-2">
+                                <input type="password" class="form-control" id="passwordLogin" placeholder="Senha">
+                            </div>
+                            <div class="d-flex justify-content-between mb-3">
+                                <div>
+                                    <input type="checkbox" class="form-check-input" id="customCheck">
+                                    <label class="form-check-label" for="customCheck">Lembrar-me</label>
+                                </div>
+                                <div>
+                                    <a class="link-secondary fst-italic" href="esqueceu-senha">Recuperar senha</a>
+                                 </div>   
+                            </div>
+                            <div class="d-grid gap-2 col-6 mx-auto">
+                                <a type="button" id="btnLogin" class="btn btn-primary btn-user btn-block">
+                                    Entrar
+                                </a>
+                            </div>
+                        </form>                                                         
                     </div>
                 </div>
             </div>
         </div>
     </div>
-
 </body>
 
-@extends('components.admin.footer')
+<script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk=" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.js" integrity="sha512-0XDfGxFliYJPFrideYOoxdgNIvrwGTLnmK20xZbCAvPfLGQMzHUsaqZK8ZoH+luXGRxTrS46+Aq400nCnAT0/w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
 
 <script src="assets/js/pages/admin/login/index.js" type="module"></script>
